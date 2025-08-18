5e8dc2daf92d189cc4447d002ecef99fd0732977
shak33 <maciej@poppek.pl>
2025-08-18 20:02:25 +0200

    Added validation to Dto

--- src/tasks/create-task.dto.ts
+++ src/tasks/create-task.dto.ts
@@ -1,7 +1,16 @@
+import { IsEnum, IsNotEmpty, IsString } from "class-validator";
 import { TaskStatusEnum } from "./task.model";
 
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsEnum(TaskStatusEnum)
   status: TaskStatusEnum;
 }
