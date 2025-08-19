071ff70ac8ba524d5640fe320bae2217adb118a3
shak33 <maciej@poppek.pl>
2025-08-19 21:01:26 +0200

    Refactorized config to use proper nestjs flow using types

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ConfigType } from './config/config.types';
+import { AppConfig } from './config/app.config';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService<ConfigType>) {}
 
   getHello(): string {
-    const prefix = this.configService.get<string>('app.messagePrefix');
-    return `${prefix} World!`;
+    const prefix = this.configService.get<AppConfig>('app');
+    return `${prefix.messagePrefix} World!`;
   }
 }
