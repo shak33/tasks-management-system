f33caa0677f49c72e668a9b5c9896b974ed82f57
shak33 <maciej@poppek.pl>
2025-08-19 20:28:18 +0200

    Extended gitignore file, added env.example file, installed nextjs/config package, added basic config module that loads data from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { appConfig } from './config/app.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    const prefix = this.configService.get<string>('app.messagePrefix');
+    return `${prefix} World!`;
   }
 }
